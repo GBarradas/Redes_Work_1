339a1db90280ba88d2cb54ed331d57ee130cc0f8
Gbarradas <gbarradas1@hotmail.com>
2022-04-26 21:25:38 +0100

    2022-04-26 21:25:38.044837

--- Teste/Server.java
+++ Teste/Server.java
@@ -4,57 +4,61 @@ import java.util.*;
 
 class Server
 {
-    private int port;
-    private  List<UserClient> clients;
-    private ServerSocket server;
+    private int port;               // Porta
+    private  List<UserClient> clients;      // lista de clientes conectados
+    private ServerSocket server;            // socket do servidor
     public Server(int port){
         this.port = port;
         clients = new ArrayList<UserClient>();
     }
     public static void main(String[] args) throws IOException
     {
-        new Server(12345).inicializeServer();
+        new Server(1234).inicializeServer();
     }
     public void inicializeServer() throws IOException
     {
-        this.server = new ServerSocket(port);
+        this.server = new ServerSocket(port);       //abre o socket na porta desejada
         System.out.println("Server connect on "+port);
         aceptClients();
     }
     public void aceptClients() throws IOException
     {
-        while(true){
-            Socket client = server.accept();
-            UserClient uc = new UserClient(client,this,clients);
+        while(true){                        //enquanto o socket estiver aberto aceita clientes
+            Socket client = server.accept();    // socket por cliente
+            UserClient uc = new UserClient(client,this,clients);    
             addClient(uc);
             new Thread(uc).start();
             
         }
     }
     
-    public void serverToAll(String msg){
-        for(UserClient a : clients){
+    public void serverToAll(String msg){        // servidor envia mensagem para todos os clientes
+        for(UserClient a : clients){            // Ex: aviso de um cliente que se ddesconectou
             a.getOutput().println("System:"+msg);
         }
     }
-    public void serverToClient(String msg, UserClient clientDestin){
-        clientDestin.getOutput().println("System: "+msg);
+    public void serverToClient(String msg, UserClient clientDestin){    // comunicaçao entre o servidor e um cliente especifico
+        clientDestin.getOutput().println("System: "+msg);               
     }
-    public void messageForAll(String msg, UserClient userSend){
-        for(UserClient a : clients){
-            a.getOutput().println(userSend+": "+msg);
+    public void messageForAll(String msg, UserClient userSend){     // mensagem para todos os clientes
+        for(UserClient a : clients){            // Ex: quando um cliente quer enviar uma mensagem para todos os clientes
+            if(a != userSend){           
+                a.getOutput().println(userSend+": "+msg);
+
+            }
         }
     }
+    //mensagem para um determianado cliente. Ex: mensagem privada
     public void messageToClient(String msg, UserClient userSend, String userDestin){
-        UserClient destin = getClient(userDestin);
-        if(destin == null){
+        UserClient destin = getClient(userDestin);              // descobre o destinatario da mensagem   
+        if(destin == null){                                 
             serverToClient(("<"+userDestin+"> not found"),userSend);
         }
         else{
             destin.getOutput().println(userSend+": -"+msg);
         }
     }
-    public UserClient getClient(String nickname){
+    public UserClient getClient(String nickname){     // devolve o userClient com o nickname do destinatario caso este exista
         for(UserClient a : clients){
             if(a.getNickname().equals(nickname)){
                 return a;
@@ -62,17 +66,14 @@ class Server
         }
         return null;
     }
-    public void removeClient(UserClient toRemove){
+    public void removeClient(UserClient toRemove){      //remove um cliente da lista de clientes conectados
         clients.remove(toRemove);
     }
-    public void addClient (UserClient newClinet){
+    public void addClient (UserClient newClinet){       // adiciona um clinte á lista de clientes conectados
         clients.add(newClinet);
     }
-    public List<UserClient> getUsers(){
-        return clients;
-    }
 }
-class ProcessUser{
+class ProcessUser{          // processa as mensagens que um determinado cliente pretende enviar
     private Server server;
     private UserClient client;
 
@@ -80,26 +81,26 @@ class ProcessUser{
         this.server = server;
         this.client = client;
     }
-    public void HandleRcivedMessages(){
+    public void HandleRcivedMessages(){         
         try{
 
             String message;
             while((message = client.getInput().readLine())!= null){
                 System.out.println(client+" "+message);
-                if(message.charAt(0) == '+'){
+                if(message.charAt(0) == '+'){     // define que é mensagem para todos
                     server.messageForAll(message, client);
                 }
-                if(message.charAt(0) == '-'){
-                    if(message.contains(" ")){
+                if(message.charAt(0) == '-'){   // define que é mensagem privada
+                    if(message.contains(" ")){  
                         int space = message.indexOf(" ");
-                        String destin = message.substring(1,space);
-                        String finalMsg = message.substring(space+1);
+                        String destin = message.substring(1,space); //obter nickname do destinatario
+                        String finalMsg = message.substring(space+1);   //obter mensagem 
                         server.messageToClient(finalMsg,client,destin);
                     }
                 }
-                if(message.charAt(0)== '#'){
+                if(message.charAt(0)== '#'){        // mensagens para o servidor
                     String command = message.substring(1);
-                    if(command.equals("Disconect")){
+                    if(command.equals("Disconect")){        // cliente deseja desconectar-se
                         server.removeClient(client);
                         server.serverToAll(client+ " Disconect");
 
@@ -129,31 +130,31 @@ class UserClient implements Runnable{
     }
     public void run() 
     {
-        try{
+        try{        //define o nickname do cliente
 
             String name = inputClient.readLine();
             outClient.println("Connected!!");
             this.nickname = name;
             System.out.println(this+" Connected!!");
             ProcessUser pu = new ProcessUser(server,this);
-            pu.HandleRcivedMessages();
+            pu.HandleRcivedMessages();             // chama o processUser para processar as mensagens enviadas pelo cliente
         }   
         catch(Exception IOException){
 
         }
     }
 
-    public String getNickname(){
+    public String getNickname(){      //devolve o nickname do cliente
         return nickname;
     }
-    public BufferedReader getInput(){
+    public BufferedReader getInput(){   //retorna o buffer de leitura do cliente
         return inputClient;
     }
-    public PrintStream getOutput(){
+    public PrintStream getOutput(){     //retorna o buffer de escrita do cliente
         return outClient;
     }
     @Override
-    public String toString(){
+    public String toString(){      
         return "<"+nickname+"> ";
     }
 
